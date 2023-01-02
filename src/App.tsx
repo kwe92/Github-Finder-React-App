1c0d421874f1c2cac49e6a02cd4aa6fa3c8dc833
Kweayon  Clark <Kweayon@gmail.com>
2023-01-01 23:49:24 -0600

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props:{})=>{
+    return(
+        <Router>
+            {/* TODO: Move to its own AppStyles.tsx */}
+        <div style={{height: "100vh", width: "100vw", background: "red", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+        <Navbar/>
+        <Footer/>
+        </div>
+        </Router>
+    );
+};
 
 export default App;
