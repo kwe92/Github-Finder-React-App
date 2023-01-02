20855cec59d0439c30cd737a90590638f30e38b6
Kweayon  Clark <Kweayon@gmail.com>
2023-01-01 23:48:42 -0600

    Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as styles from "./FooterStyles"
+
+const Footer = (props:{})=>{
+    return(
+        <styles.StyledFooter></styles.StyledFooter>
+    );
+};
+
+export default Footer;
