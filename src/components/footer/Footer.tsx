7d38b3edb4f97b10228ec3aaa95015b4246578a3
Kweayon  Clark <Kweayon@gmail.com>
2023-01-02 15:36:36 -0600

    Responsive Fotter

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import * as styles from "./FooterStyles"
+import {FooterContentWrapper as FooterWraper, CopyrightContent, HashTag, Footer as StyledFooter, CopyrightIcon} from "./FooterStyles";
 
-const Footer = (props:{})=>{
-    return(
-        <styles.StyledFooter></styles.StyledFooter>
-    );
+const Footer = (props:{}) => {
+
+    const footerContent = 
+        <FooterWraper>
+            <HashTag/>
+            <CopyrightContent>Copyright <span><CopyrightIcon/></span> 2023 All rights reserved.</CopyrightContent>
+        </FooterWraper>
+
+    const footer =  
+        <StyledFooter>
+            {footerContent}
+        </StyledFooter>;
+
+    return footer;
 };
 
 export default Footer;
