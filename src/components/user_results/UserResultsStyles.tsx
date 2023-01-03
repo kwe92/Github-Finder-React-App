f098bbfbf103918af6571d9556f617afc6b6f5a5
Kweayon  Clark <Kweayon@gmail.com>
2023-01-02 18:15:29 -0600

    User List Grid Component

---
+++ src/components/user_results/UserResultsStyles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const GridWrapper = styled.div`
+
+color: white;
+
+display: grid;
+
+grid-template-columns: auto auto auto;
+
+overflow: scroll;
+
+padding-left: 2rem;
+
+`;
