d9bfcfcce5c0b956c8c7461c8121a2dedaaf1f96
Kweayon  Clark <Kweayon@gmail.com>
2023-01-02 18:14:22 -0600

    API Fetch Custom Hook

---
+++ src/components/custom_hooks/useUsers.tsx
@@ -0,0 +1,25 @@
+import React,{ useEffect, useState } from "react";
+import {GITHUB_API_URL, GITHUB_API_TOKEN} from "../../private/ApiKey"
+import axios from "axios";
+
+export const useUser = () => {
+    const [user, SetUser] = useState([]);
+
+    const fetchUsers = async () => {
+        const data = await axios.get(
+             `${GITHUB_API_URL}/users`    
+        ,
+        { headers: {
+            Authorization: GITHUB_API_TOKEN
+        }});
+
+        const result = data.data;
+
+        SetUser(result);
+
+    };
+
+    useEffect(() => {fetchUsers()},[]);
+
+    return [user, SetUser] as Array<any>;
+}
