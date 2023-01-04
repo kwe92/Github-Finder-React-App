ce7577bfb12472a88359df06e9de0a32c676c42d
Kweayon  Clark <Kweayon@gmail.com>
2023-01-03 23:26:36 -0600

    Update

--- src/components/user_results/UserResultsStyles.tsx
+++ src/components/user_results/UserResultsStyles.tsx
@@ -1,15 +1,36 @@
 import styled from "styled-components";
+import {ListTile} from "../user_item/UserItemStyles"
 
-export const GridWrapper = styled.div`
+interface _props{
+    isLoading: boolean;
+}
+
+export const GridWrapper = styled.div<_props>`
 
 color: white;
 
+justify-content: ${props => props.isLoading && "center"};
+
+align-items: ${props => props.isLoading && "center"};
+
 display: grid;
+
+height: 100%;
 
 grid-template-columns: auto auto auto;
 
 overflow: scroll;
 
-padding-left: 2rem;
+padding: 2rem;
+
+@media(max-width:768px){
+
+    grid-template-columns: auto auto;
+
+}
+
+@media(max-width:450px){
+    grid-template-columns:auto;
+}
 
 `;
