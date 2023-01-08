05ce58415b8e386b35e9e54056c0d6beba975c3e
Kweayon  Clark <Kweayon@gmail.com>
2023-01-07 20:50:00 -0600

    Global Styles

---
+++ src/indexStyles.tsx
@@ -0,0 +1,30 @@
+import {createGlobalStyle,GlobalStyleComponent,DefaultTheme} from "styled-components";
+import {ReactNode,ReactElement} from "react"
+
+// Styled COmponent Helper Funtion to create a base global style.
+
+export const AppGlobalTheme: GlobalStyleComponent<{}, DefaultTheme> = 
+    createGlobalStyle`
+
+    * {
+
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+    }
+
+    body {
+    
+        font-family: Roboto;
+        background: #191d24;
+
+    }
+
+    li {
+
+        list-style: none;
+
+    }
+
+`;
