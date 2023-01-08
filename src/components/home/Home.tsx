742e75899e683a5bca38daa12c7705d50d7c5f63
Kweayon  Clark <Kweayon@gmail.com>
2023-01-08 17:52:27 -0600

    Update

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { SetState } from "../../types/state/stateTypes";
 import { UserResults } from "../user_results/UserResults";
 import UserSearch from "../user_search/UserSearch";
 
-const Home = (props:{}) => 
+const Home = (props:{}): JSX.Element => 
+            {
+                const [userInput, setUserInput]: [string, SetState<string>] = useState("");
+                console.log("User Input",userInput)
+            return (
             <>
-            <UserSearch/>
-            <UserResults/>
+            <UserSearch setSearch = {setUserInput}/>
+            {userInput.trim() && <UserResults searchResults={userInput}/>}
             </>
+            );
+            };
 
 
 export default Home;
