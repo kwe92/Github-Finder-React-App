68d691b5f97f9fe87e396777beca78ef0655c7fb
Kweayon  Clark <Kweayon@gmail.com>
2023-01-07 20:46:10 -0600

    Refactor

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { UserResults } from "../user_results/UserResults";
+import UserSearch from "../user_search/UserSearch";
+
+const Home = (props:{}) => 
+            <>
+            <UserSearch/>
+            <UserResults/>
+            </>
 
 
-export const Home = (props:{}) => {
-    return (<UserResults/>);
-};
-
+export default Home;
