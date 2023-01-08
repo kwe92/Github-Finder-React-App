db99d2e4c293de1f63dd01d82aa63237caa48c70
Kweayon  Clark <Kweayon@gmail.com>
2023-01-07 20:44:43 -0600

    Refactor

--- src/components/custom_hooks/useUsers.tsx
+++ src/components/custom_hooks/useUsers.tsx
@@ -2,18 +2,20 @@ import React,{ useEffect, useState } from "react";
 import {GITHUB_API_URL, GITHUB_API_TOKEN} from "../../private/ApiKey"
 import axios from "axios";
 
-export const useUser = () => {
+const useUser: Function = () => {
     const [user, SetUser] = useState([]);
 
     const fetchUsers = async () => {
         const data = await axios.get(
-             `${GITHUB_API_URL}/users`    
+             `${GITHUB_API_URL}/search/users?q=kw`    
         ,
         { headers: {
             Authorization: GITHUB_API_TOKEN
         }});
 
-        const result = data.data;
+        console.log(data);
+
+        const result = data.data.items;
 
         SetUser(result);
 
@@ -21,5 +23,8 @@ export const useUser = () => {
 
     useEffect(() => {fetchUsers()},[]);
 
-    return [user, SetUser] as Array<Array<Object>>;
-}
+    return [user, SetUser] as Array<Array<object>>;
+
+};
+
+export default useUser;
