75c5066f012ce1fab354076477be2d302ad6b5e0
Kweayon  Clark <Kweayon@gmail.com>
2023-01-07 20:43:45 -0600

    Update

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,37 +1,31 @@
-import React from "react";
+import React, {FunctionComponent} from "react";
 import {AboutContentWrapper, Title, ContentBody, ContainerWrapper, ContentContainer} from './AboutStyles';
 
-const About = (props:{}) => {
 
-    const title = 
-        <Title>Github Finder</Title>;
-
-    const content = [{name:"Version", content:"1.0.0"},{name:"Developed By:", content:"Kweayon Clark"},{name:"Main Technology Used:", content:"Styled Components && Typescript"}];
-    
-    const [version, developer, mainTech] = content.map((content)=>
-        <ContainerWrapper>
-            <ContentContainer>
-                {content["name"]} 
-            </ContentContainer>
-            <ContentContainer Secondary>
-                {content["content"]}
-            </ContentContainer>
-        </ ContainerWrapper>
-        );
-
-
-    const contentBody = 
-        <ContentBody>A React app to search GitHub profiles and see profile details.</ContentBody>;
+const About: FunctionComponent = (props:{}) => {
 
     return (
         <AboutContentWrapper>
-            {title}
-            {contentBody}
+            <Title>Github Finder</Title>
+            <ContentBody>A React app to search GitHub profiles and see profile details.</ContentBody>;
             {version}
             {developer}
             {mainTech}
         </AboutContentWrapper>
     );
 };
+
+const content = [{name:"Version", content:"1.0.0"},{name:"Developed By:", content:"Kweayon Clark"},{name:"Main Technology Used:", content:"Styled Components && Typescript"}];
+    
+const [version, developer, mainTech]: Array<JSX.Element> = content.map((content)=>
+    <ContainerWrapper>
+        <ContentContainer>
+            {content["name"]} 
+        </ContentContainer>
+        <ContentContainer secondary>
+            {content["content"]}
+        </ContentContainer>
+    </ ContainerWrapper>
+    );
 
 export default About;
