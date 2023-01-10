91c5fbbc2341eb29bac1c614356f0cb29d519e7a
Kweayon  Clark <Kweayon@gmail.com>
2023-01-09 23:47:27 -0600

    Update

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,23 +1,27 @@
 import { stat } from "fs";
-import React, { useState } from "react";
+import React, { useState, FunctionComponent } from "react";
 import { SetState } from "../../types/state/stateTypes";
 import { UserResults } from "../user_results/UserResults";
 import UserSearch from "../user_search/UserSearch";
 import { ErrorMessage } from "./HomeStyles";
 
-const Home = (props:{}): JSX.Element => 
+const Home: FunctionComponent  = (props:{}): JSX.Element => 
             {
                 const [userInput, setUserInput]: [string, SetState<string>] = useState("");
+
                 const [error, setError]: [boolean, SetState<boolean>] = useState(false);
-                const setState = (state: string, error: boolean)=>{
-                    setUserInput(state);
-                    setError(error);
-                };
+
+                const setState = (state: string, error: boolean) => 
+                    {
+                        setUserInput(state);
+                        setError(error);
+                    };
+
                 console.log("User Input",userInput)
+
             return (
                 <>
             <div style={{display:"flex", flexDirection: "column", alignItems: "center", paddingBottom: "1rem"
-            // , background: "green"
         }}>
             {error && <ErrorMessage>Please enter a name</ErrorMessage>}
             <UserSearch setSearch = {setState}/>
