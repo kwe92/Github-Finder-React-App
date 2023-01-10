77d6b8d47b02dbeb59499a6d35f6233065fddb6e
Kweayon  Clark <Kweayon@gmail.com>
2023-01-09 23:46:40 -0600

    Update

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,14 +1,16 @@
 import React, {FunctionComponent} from "react";
 import {AboutContentWrapper, Title, ContentBody, ContainerWrapper, ContentContainer} from './AboutStyles';
 
-const About: FunctionComponent = (props:{}) => 
+const About: FunctionComponent = (props:{}): JSX.Element => 
         <AboutContentWrapper>
             <Title>Github Finder</Title>
-            <ContentBody>A React app to search GitHub profiles and see profile details.</ContentBody>;
+            <ContentBody>A React app to search github profiles and see profile details.</ContentBody>
             {version}
             {developer}
             {mainTech}
         </AboutContentWrapper>;
+
+// Implementation Details
 
 const content: {name: string; content: string;}[] = [{name:"Version", content:"1.0.0"},{name:"Developed By:", content:"Kweayon Clark"},{name:"Main Technology Used:", content:"Styled Components && Typescript"}];
     
