453e868a8efecf92167c34d19ec922b011eb0ef9
Kweayon  Clark <Kweayon@gmail.com>
2023-01-11 13:55:02 -0600

    Update

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -3,15 +3,17 @@ import { NavigateFunction, useLocation, useNavigate } from "react-router-dom";
 import { SetState } from "../../types/state/stateTypes";
 import useRepos from "../custom_hooks/useRepos";
 import useUser from "../custom_hooks/useUser";
-import {MainContainer, TextIcon, ProfileImage, NameImageContainer,UserName, UserNameContainer, UserLogin, DescriptionContainer, NameContentContainer, UserBadge, Bio, Row, ProfileUrlButton, LocationInfoContainer, ListTile, ListTileContentTop, ListTileContentBottom, VerticalLine, IconListTileContainer, IconUsers, Column, HireableBadge, ImageDescriptionContainer, IconListTileContainerWrapper, MainInnerContainer, LogoIconVerticalLine, ListTileRepoContaner, ListTileRepoItem, BadgeRow, IconBadge} from "./UserStyles";
+import {MainContainer, TextIcon, ProfileImage, NameImageContainer,UserName, UserNameContainer, UserLogin, DescriptionContainer, NameContentContainer, UserBadge, Bio, Row, ProfileUrlButton, LocationInfoContainer, ListTile, ListTileContentTop, ListTileContentBottom, VerticalLine, IconListTileContainer, IconUsers, Column, HireableBadge, ImageDescriptionContainer, IconListTileContainerWrapper, MainInnerContainer, LogoIconVerticalLine, ListTileRepoContaner, ListTileRepoItem, BadgeRow, IconBadge, IconUserFriends, IconBox, IconInBox, IconEye, IconStar, IconInfo, IconFork, IconListTileRepo, IconLink, RepoTitle, RepoHeader} from "./UserStyles";
 
 // TODO: Replace all the duplicate icons
 // TODO: Fix the footer not completely disappearing
-// TODO: MAke navbar and footer disapear on user scroll down??
+// TODO: Make navbar and footer disapear on user scroll down??
 // TODO: Fix alignment of middle section icons when the display is changed to grid
 // TODO: Finish styling the badges for the user page
 // TODO: Handle navigation onClick to the respecting clicked upon repository named in the ListTile
 // TODO: Look for other UI errors and clean code errors
+// TODO: Change <a> redirect to a new tab
+// TODO: Round the IconBadgeContainer and add a little padding to it
 
 const User: FunctionComponent = (props:{}): JSX.Element => {
     
@@ -21,34 +23,43 @@ const User: FunctionComponent = (props:{}): JSX.Element => {
 
     const [repos, setRepos] = useRepos(state);
 
+    console.log("User: ",user);
     console.log("REPOS: ",repos);
+    console.log("Watchers_count: ",repos["watchers_count"]);
+
 
     const repoListItems = repos.map((repo: any) => {
         return <li key ={repo["id"]}>
             <ListTileRepoItem>
 
-                {repo["name"]}
-                
+                <IconListTileRepo href={repo["html_url"]}>
+
+                    <IconLink/>
+                    <RepoTitle>
+                        {repo["name"]}
+                    </RepoTitle>
+
+                </IconListTileRepo>
                 <BadgeRow>
-
-                    <IconBadge>
-                        <IconUsers/>
-                        <p>24</p>
+                    
+                    <IconBadge primary={"#00b0ff"} secondary = {"rgba(0, 176, 255, 0.2)"}>
+                        <IconEye/>
+                        <p>{repo["watchers_count"]}</p>
                     </IconBadge>
 
                     <IconBadge>
-                        <IconUsers/>
-                        <p>24</p>
+                        <IconStar/>
+                        <p>{repo["stargazers_count"]}</p>
                     </IconBadge>
 
                     <IconBadge>
-                        <IconUsers/>
-                        <p>24</p>
+                        <IconInfo/>
+                        <p>{repo["open_issues_count"]}</p>
                     </IconBadge>
 
                     <IconBadge>
-                        <IconUsers/>
-                        <p>24</p>
+                        <IconFork/>
+                        <p>{repo["forks_count"]}</p>
                     </IconBadge>
 
                 </BadgeRow>
@@ -151,7 +162,7 @@ const User: FunctionComponent = (props:{}): JSX.Element => {
                                 <ListTileContentBottom>{user["following"]}</ListTileContentBottom>
                             </ListTile>
 
-                            <IconUsers/>
+                            <IconUserFriends/>
 
                         </IconListTileContainer>
 
@@ -164,7 +175,7 @@ const User: FunctionComponent = (props:{}): JSX.Element => {
                                 <ListTileContentBottom>{user["public_repos"]}</ListTileContentBottom>
                             </ListTile>
 
-                            <IconUsers/>
+                            <IconBox/>
 
                         </IconListTileContainer>
 
@@ -177,7 +188,7 @@ const User: FunctionComponent = (props:{}): JSX.Element => {
                                     <ListTileContentBottom>{user["public_gists"]}</ListTileContentBottom>
                                 </ListTile>
 
-                            <IconUsers/>
+                            <IconInBox/>
 
                         </IconListTileContainer>
 
@@ -185,9 +196,9 @@ const User: FunctionComponent = (props:{}): JSX.Element => {
 
             </MainInnerContainer>
 
-            <h1 style={{color: "white"}}>Latest Repositories</h1>
+            <RepoHeader>Latest Repositories</RepoHeader>
 
-            <ListTileRepoContaner style={{color: "white"}}>
+            <ListTileRepoContaner>
                 {repoListItems}
             </ListTileRepoContaner>
 
