585204870e296529587e4638ac164822cb078d11
Kweayon  Clark <Kweayon@gmail.com>
2023-01-13 09:04:06 -0600

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Footer from "./components/footer/Footer";
@@ -6,20 +6,25 @@ import About from "./components/about/About";
 import Wrapper, {Container} from "./AppStyles";
 import { Notfound } from "./components/notfound/Notfound";
 import Home  from "./components/home/Home";
-import styled from "styled-components";
 import User from "./components/User/User";
 
+//TODO: Change the isScrolling Variable | implemented differently | displays on user navigation
 const App = (props:{}) => 
            {
+
+            const [isScrolling, setIsScrolling] = useState(false);
+
+            // console.log("Scroll Y",document.scrollingElement?.scrollHeight);
+
             return (
                 <Router>
-                    <Navbar/>
-                    <Wrapper>
+                    <Navbar setFooter = {setIsScrolling}/>
+                    <Wrapper isScrolling = {isScrolling}>
                     <Container>
                         <Routes>
                             <Route path="/" element={<Home/>}/>
                             <Route path="/about" element={<About/>}/>
-                            <Route path="/user/:login" element={<User/>}/>
+                            <Route path="/user/:login" element={<User setScrolling = {setIsScrolling}/>}/>
                             <Route path="/*" element={<Notfound/>}/>
                         </Routes>
                         </Container>
